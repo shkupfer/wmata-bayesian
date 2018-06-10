763979676d0c8f7b0b29e8db681417558fd30e60
shkupfer <shkupfer@ncsu.edu>
2018-06-10 13:54:56 -0400

    While loop

--- pull_data/api_to_text.py
+++ pull_data/api_to_text.py
@@ -16,22 +16,23 @@ if __name__ == "__main__":
     headers = {'api_key': args.wmata_api_key}
     params = urllib.urlencode({})
 
-    tz = pytz.timezone(args.timezone)
-    tz_now = datetime.datetime.now(tz).strftime('%Y-%m-%d_%H:%M:%S')
+    while True:
+        tz = pytz.timezone(args.timezone)
+        tz_now = datetime.datetime.now(tz).strftime('%Y-%m-%d_%H:%M:%S')
 
-    try:
-        conn = httplib.HTTPSConnection('api.wmata.com')
-        conn.request("GET", "/TrainPositions/TrainPositions?contentType={contentType}&%s" % params, "{body}", headers)
-        response = conn.getresponse()
-        data = response.read()
-        print "{0}: Successfully pulled data".format(tz_now)
-        conn.close()
-    except Exception as e:
-        data = "Uh oh, there was an error pulling the data"
-        print "{0}: {1}".format(tz_now, data)
-        print "[Errno {0}] {1}".format(e.errno, e.strerror)
+        try:
+            conn = httplib.HTTPSConnection('api.wmata.com')
+            conn.request("GET", "/TrainPositions/TrainPositions?contentType={contentType}&%s" % params, "{body}", headers)
+            response = conn.getresponse()
+            data = response.read()
+            print "{0}: Successfully pulled data".format(tz_now)
+            conn.close()
+        except Exception as e:
+            data = "Uh oh, there was an error pulling the data"
+            print "{0}: {1}".format(tz_now, data)
+            print "[Errno {0}] {1}".format(e.errno, e.strerror)
 
-    outfile_path = os.path.join(args.output_dir, tz_now)
-    outfile = open(outfile_path, 'w')
-    outfile.write(data)
-    outfile.close()
+        outfile_path = os.path.join(args.output_dir, tz_now)
+        outfile = open(outfile_path, 'w')
+        outfile.write(data)
+        outfile.close()
